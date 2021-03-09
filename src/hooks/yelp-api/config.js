78a724e9ac89bb7add2acfc604e6caea93e12c08
Leonel <leonel@Leonels-MacBook-Pro.local>
2021-03-08 22:58:19 -0500

    testing new cors

--- src/hooks/yelp-api/config.js
+++ src/hooks/yelp-api/config.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3';
+// export const API_BASE_URL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3';
+export const API_BASE_URL = 'https://test.cors.workers.dev/?https://api.yelp.com/v3';
 
 // FIXME: remove this token
 // NOTE: as long as this token is saved in the frontend, the application is not production ready.
